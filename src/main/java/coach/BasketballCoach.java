a6b1aa93455edc583706c416d1f1f52184bdf246
Bonum-Ursus <Viktor.Evtyukhin@mail.ru>
2020-02-13 15:39:13 +0500

    BasketballCoach class added

---
+++ src/main/java/coach/BasketballCoach.java
@@ -0,0 +1,8 @@
+package coach;
+
+public class BasketballCoach implements Coach{
+    @Override
+    public String getDailyWorkout() {
+        return "Throw ball in basket for 100 times.";
+    }
+}
