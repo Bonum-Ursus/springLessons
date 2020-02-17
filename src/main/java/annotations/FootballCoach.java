29209934bd1ac80b8695fa54bec5f76afa521cea
Bonum-Ursus <Viktor.Evtyukhin@mail.ru>
2020-02-17 17:45:52 +0500

    PostConstruct and PreDestroy method annotation

--- src/main/java/annotations/FootballCoach.java
+++ src/main/java/annotations/FootballCoach.java
@@ -5,18 +5,23 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component
-@Scope("prototype")
+@Scope() //by default singleton. Can change on "prototype".
 public class FootballCoach implements Coach{
 
-    @Autowired
-    @Qualifier("oneMoreFortuneService")
-    private FortuneService fortuneService;
-//    By constructor
 //    @Autowired
-//    public FootballCoach(FortuneService fortuneService) {
-//        this.fortuneService = fortuneService;
-//    }
+//    @Qualifier("oneMoreFortuneService")
+    private FortuneService fortuneService;
+
+//    By constructor
+    @Autowired
+    public FootballCoach(@Qualifier("oneMoreFortuneService")FortuneService fortuneService) {
+        System.out.println("Constructor");
+        this.fortuneService = fortuneService;
+    }
 
     @Override
     public String getDailyWorkout() {
@@ -32,4 +37,14 @@ public class FootballCoach implements Coach{
 //    public void setFortuneService(FortuneService fortuneService){
 //        this.fortuneService = fortuneService;
 //    }
+
+    @PostConstruct
+    public void construct(){
+        System.out.println("PostConstruct method");
+    }
+
+    @PreDestroy
+    public void destroy(){
+        System.out.println("PreDestroy method");
+    }
 }
